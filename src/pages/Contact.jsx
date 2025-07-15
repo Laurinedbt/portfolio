d4e4c6db575a767c9d667469e44df5353e9f71d1
Laurine <deberdt.laurine@gmail.com>
2025-07-15 18:37:46 +0200

    Add contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,37 @@
 const Contact = () => {
     return (
         <div>
-            
+            <div>
+                <h1>Contact</h1>
+                <p>Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</p>
+            </div>
+            <section>
+                <h2>Formulaire de contact</h2>
+                    <form action="index.html" method="post">
+                        <div className="mb-2">
+                            <label for="nom" className="visually-hidden">Votre nom</label>
+                            <input type="text" className="form-control" id="nom" placeholder="Votre nom"/>
+                        </div>
+                        <div className="mb-2">
+                            <label for="email" className="visually-hidden">Votre adresse email</label>
+                            <input type="email" className="form-control" id="email" placeholder="Votre adresse email"/>
+                        </div>
+                        <div className="mb-2">
+                            <label for="phone" className="visually-hidden">Votre numéro de téléphone</label>
+                            <input type="tel" className="form-control" id="phone" placeholder="Votre numéro de téléphone"/>
+                        </div>
+                        <div className="mb-2">
+                            <label for="nom" className="visually-hidden">Sujet</label>
+                            <input type="text" className="form-control" id="" placeholder="Sujet"/>
+                        </div>
+                        <div className="mb-2">
+                            <label for="message" className="visually-hidden">Votre message</label>
+                            <textarea style={{ height: "500px"}} className="form-control" id="message" placeholder="Votre message" required></textarea>
+                        </div>
+
+                    </form>
+                <h2>Mes coordonnées</h2>
+            </section>
         </div>
     )
 }
