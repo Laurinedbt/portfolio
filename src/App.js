007524ecb88750bc5869a1d913b2af55eaf6c13d
Laurine <deberdt.laurine@gmail.com>
2025-07-15 16:34:33 +0200

    Ajout React router et fichiers jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
 
-      </header>
     </div>
   );
 }
+
+<Routes>
+  <Route path= "/" element={Home}></Route>
+  <Route path= "/about" element={About}></Route>
+  <Route path= "/contact" element={Contact}></Route>
+  <Route path= "/portfolio" element={Portfolio}></Route>
+  <Route path= "/services" element={Services}></Route>
+</Routes>
 
 export default App;
