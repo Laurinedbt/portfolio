cd549411a8745c60a9d5b9d93fe118e663b9b9c8
Laurine <deberdt.laurine@gmail.com>
2025-07-17 15:49:07 +0200

    Ajout Bootsrap Icons et icônes dans le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,16 +8,21 @@ function Footer () {
           
           {/* Colonne 1 - Contact */}
           <div className="col-md-4 mb-4">
-            <p>John Doe</p>
+            <h3>John Doe</h3>
             <p className="mb-0">40 rue Laure Diebold</p>
             <p className="mb-0">69009 Lyon, France</p>
             <p className="mb-0"><a href="tel:1020304050">10 20 30 40 50</a></p>
             <p className="mb-0"><a href="mailto:john.doe@gmail.com">john.doe@gmail.com</a></p>
+            <div className="mt-4">
+                <a href="https://github.com/Laurinedbt/portfolio" target="_blank" rel="noopener noreferrer"><i className="bi bi-github"></i></a>
+                <a href="https://x.com/" target="_blank" rel="noopener noreferrer"><i className="bi bi-twitter"></i></a>
+                <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i className="bi bi-linkedin"></i></a>
+            </div>
           </div>
           
           {/* Colonne 2 - Liens */}
           <div className="col-md-4 mb-4">
-            <p>Liens utiles</p>
+            <h3>Liens utiles</h3>
             <ul className="list-unstyled">
               <li><a href="/" className="text-light text-decoration-none">Accueil</a></li>
               <li><a href="/services" className="text-light text-decoration-none">Services</a></li>
@@ -29,7 +34,7 @@ function Footer () {
           
           {/* Colonne 3 - Dernières réalisations */}
           <div className="col-md-4 mb-4">
-            <p>Mes dernières réalisations</p>
+            <h3>Mes dernières réalisations</h3>
             <ul className="list-unstyled">
                 <li><a href="/portfolio" className="text-light text-decoration-none">Fresh Food</a></li>
                 <li><a href="/portfolio" className="text-light text-decoration-none">Restaurant Akira</a></li>
