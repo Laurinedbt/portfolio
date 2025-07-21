fee20ba0d6e321503fd6e4b8a295640ee8159000
Laurine <deberdt.laurine@gmail.com>
2025-07-21 18:34:36 +0200

    Correction responsive image fond header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,11 @@ const Home = () => {
     return (
         <div>
             <header className="home-header">
-                <h1 className="">Bonjour, je suis John Doe</h1>
-                <h2>Développeur web full stack</h2>
-                <button type="button" className="btn btn-danger px-4 mt-2">En savoir plus</button>
+                <div className="hero-content text-center">
+                    <h1 className="">Bonjour, je suis John Doe</h1>
+                    <h2>Développeur web full stack</h2>
+                    <button type="button" className="btn btn-danger px-4 mt-2">En savoir plus</button>
+                </div>
             </header>
             <main>
                 <div class="modal" tabindex="-1">
